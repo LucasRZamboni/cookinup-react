918f2b97aade90058029c6a4a86bb8f47034d337
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-03 12:47:55 -0300

    feat: header.css and index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,58 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="IMG/Logo1.png" alt="logo"/>
+        <ul id="nav-menu">
+          <li>
+            <a href="#home" className="active">
+              Home
+            </a>
+          </li>
+          <li className="li-receitas">
+            <a href="#" className="dropdown-link">
+              Receitas <ion-icon name="chevron-down-outline"></ion-icon>
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a href="#doces">Doces</a>
+              </li>
+              <li>
+                <a className="dropdown-link">Salgado</a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a href="#carnes">Carnes</a>
+                  </li>
+                  <li>
+                    <a href="#massas">Massas</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#sobre">Sobre nós</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+        <div className="search">
+          <button type="button">
+            <ion-icon name="search-outline"></ion-icon>
+          </button>
+          <input
+            type="search"
+            name="search"
+            id="pesquisa"
+            placeholder="Pesquise aqui..."
+          />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
