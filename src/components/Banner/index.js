dd3945eea2cf040e66ebe83aea67ad243bc050c1
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-03 12:48:13 -0300

    feat: banner.css and index.js

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="IMG/banner.png" alt="banner da pagina"/>
+    </div>
+  );
+};
+
+export default Banner;
