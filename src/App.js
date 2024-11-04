c6fc72ebec6f0770f6e087d61743bac3b12a0fd7
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 20:24:09 -0300

    feat: divisor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Divisor from "./components/Divisor";
 function App() {
   return (
     <div id="home">
       <Header />
       <Banner />
+      <Divisor />
       <Footer />
     </div>
     
