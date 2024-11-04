401c508d1e2a1ebd42df60e5f29a6b44d83b8034
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 20:23:59 -0300

    feat: divisor.css index.js

---
+++ src/components/Divisor/index.js
@@ -0,0 +1,22 @@
+import './Divisor.css';
+
+const Divisor = () => {
+    return(
+        <div className='divisor'>
+            <div>
+                <img src='IMG/ÍconeOleos.png' alt='icone óleos' />
+                <p>Óleos</p>
+            </div>
+            <div>
+                <img src='IMG/ÍconeFrutasSecas.png' alt='icones cereais' />
+                <p>Cereais</p>
+            </div>
+            <div>
+                <img src='IMG/ÍconeLaticínios.png' alt='icone leites' />
+                <p>Leites</p>
+            </div>
+        </div>
+    );
+};
+
+export default Divisor;
