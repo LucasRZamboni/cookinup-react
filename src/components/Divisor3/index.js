ee133b5f0f4c4658e3cb1d27e40c335808b1411b
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 22:01:43 -0300

    feat: divisor3

---
+++ src/components/Divisor3/index.js
@@ -0,0 +1,25 @@
+import './Divisor3.css';
+
+const Divisor3 = () => {
+    return(
+        <div className='divisor3'>
+            <div>
+                {/* <img src='IMG/ÍconeFrutass.png' alt='icone frutas' /> */}
+                <div className='itens'></div>
+                <p>Frutas</p>
+            </div>
+            <div>
+                {/* <img src='IMG/ÍconeFarinhas.png' alt='icones farinhas' /> */}
+                <div className='itens'></div>
+                <p>Farinhas</p>
+            </div>
+            <div>
+                {/* <img src='IMG/ÍconeLeguminosas.png' alt='icone legumes' /> */}
+                <div className='itens'></div>
+                <p>Legumes</p>
+            </div>
+        </div>
+    );
+};
+
+export default Divisor3;
