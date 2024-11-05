ec0289c02f6ef8e5a3fad67b0d7227b4335b9ff9
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 22:02:07 -0300

    feat: middlepage3

---
+++ src/components/MiddlePage3/index.js
@@ -0,0 +1,19 @@
+import './MiddlePage3.css';
+
+const MiddlePage3 = () => {
+    return (
+        <section className='mp3'>
+            <div>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, iusto hic quam autem voluptates harum, eveniet nihil est delectus sit doloribus, consequuntur cupiditate libero corporis eius. Labore enim facere ab?  
+                </p>
+            </div>
+            {/* <img src='IMG/Cooking-Up-IMG03.png' alt='pessoa cozinhando'/> */}
+            <div className='image'>
+
+            </div>
+        </section>
+    );
+};
+
+export default MiddlePage3;
