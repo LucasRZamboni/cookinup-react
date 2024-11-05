5a250a5fc202c0729d4fe154bec16eb22f0d2580
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 22:01:27 -0300

    feat: divisior2

--- src/components/Divisor2/index.js
+++ src/components/Divisor2/index.js
@@ -1,19 +1,22 @@
-import './Divisor.css';
+import './Divisor2.css';
 
 const Divisor2 = () => {
     return(
-        <div className='divisor'>
+        <div className='divisor2'>
             <div>
-                <img src='IMG/ÍconeOleos.png' alt='icone óleos' />
-                <p>Óleos</p>
+                {/* <img src='IMG/ÍconePães.png' alt='icone pães' /> */}
+                <div className='itens'></div>
+                <p>Pães</p>
             </div>
             <div>
-                <img src='IMG/ÍconeFrutasSecas.png' alt='icones cereais' />
-                <p>Cereais</p>
+                {/* <img src='IMG/ÍconeTemperos.png' alt='icones temperos' /> */}
+                <div className='itens'></div>
+                <p>Temperos</p>
             </div>
             <div>
-                <img src='IMG/ÍconeLaticínios.png' alt='icone leites' />
-                <p>Leites</p>
+                {/* <img src='IMG/ÍconeDoces.png' alt='icone doces' /> */}
+                <div className='itens'></div>
+                <p>Doces</p>
             </div>
         </div>
     );
