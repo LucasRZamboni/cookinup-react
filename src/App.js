d8a8b83f138d338c29b826fe140ba0de1e721528
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 22:02:46 -0300

    feat: middlepage divisor2 middlepage2 divisor3 middlepage3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Divisor from "./components/Divisor";
+import MiddlePage from "./components/MiddlePage";
+import MiddlePage2 from "./components/MiddlePage2";
+import Divisor2 from "./components/Divisor2";
+import Divisor3 from "./components/Divisor3";
+import MiddlePage3 from "./components/MiddlePage3";
 function App() {
   return (
     <div id="home">
       <Header />
       <Banner />
       <Divisor />
+      <MiddlePage />
+      <Divisor2 />
+      <MiddlePage2 />
+      <Divisor3 />
+      <MiddlePage3 />
       <Footer />
     </div>
-    
   );
 }
 
