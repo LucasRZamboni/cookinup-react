b71eeccf610ea68a3af5bd51645454c67af5102b
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-11-04 21:18:01 -0300

    feat: middlepage2.css index.js

---
+++ src/components/MiddlePage2/index.js
@@ -0,0 +1,20 @@
+import './MiddlePage2.css';
+
+const MiddlePage2 = () => {
+    return (
+        <section>
+            
+            {/* <img src='IMG/Cooking-Up-IMG03.png' alt='pessoa cozinhando'/> */}
+            <div className='image'>
+
+            </div>
+            <div>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, iusto hic quam autem voluptates harum, eveniet nihil est delectus sit doloribus, consequuntur cupiditate libero corporis eius. Labore enim facere ab?  
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default MiddlePage2;
